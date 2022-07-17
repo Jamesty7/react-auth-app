4f7495c893ba7c1e6b46f970b49aa23b1bfb8690
Jamesty7 <jamestysoto@gmail.com>
2022-07-17 14:07:28 +0200

    first commit for React auth app frontend

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+import Home from './Home';
+import Login from './Login';
+import SignUp from './SignUp';
+
+function App() {
+  return 
+    <div className="App"></div>
+  
+}
+
+export default App;
+
+
+
